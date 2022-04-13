8ee2332b4180409266332608b9b12e36edfc85c1
Mr._Theophilus <yeshua.t137@gmail.com>
2022-04-13 02:00:17 +0000

    updated databse to [db.sqlite3]

--- Ocean_Library/settings.py
+++ Ocean_Library/settings.py
@@ -80,13 +80,14 @@ WSGI_APPLICATION = 'Ocean_Library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
+"""
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -97,6 +98,8 @@ DATABASES = {
             'POST': '5432',
         }
 }
+"""
+
 
 
 # Password validation
